40eca58f1bfa28bac90a2d93ffde732573710a14
Swapnil Wagh <61085075+SwapnilWagh06@users.noreply.github.com>
2026-07-04 19:41:42 +0530

    Time: 682 ms (68.28%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,3 @@
+select Department,Employee,Salary from(
+select *,dense_rank() over(partition by B.Department order by salary desc)rank1 from(SELECT name Employee,salary,departmentid FROM Employee)A
+LEFT JOIN(SELECT id,name Department FROM Department)B ON A.departmentId =B.ID )t where rank1=1
